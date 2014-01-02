95946445d5cfd0fcd06fedad22ef45670d13fbf9
Tanner Rogalsky <the.t.dawg@gmail.com>
2014-01-02 17:09:56 -0500

    make the LQDB check recursive

--- gendarme/rules/Unity.Rules.Performance/AvoidWritingToTheDatabaseOutsideOfSceneChangesRule.cs
+++ gendarme/rules/Unity.Rules.Performance/AvoidWritingToTheDatabaseOutsideOfSceneChangesRule.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Globalization;
 using System.Linq;
+using System.Collections.Generic;
 
 using Gendarme.Framework;
 
@@ -23,28 +24,48 @@ namespace Unity.Rules.Performance {
         return RuleResult.DoesNotApply;
       }
 
+      if (method.HasBody == false) {
+        return RuleResult.DoesNotApply;
+      }
+
       if (methodNames.Contains(method.Name)) {
-        if (method.HasBody == false) {
-          return RuleResult.DoesNotApply;
+        HashSet<MethodDefinition> seenMethods = new HashSet<MethodDefinition>();
+        this.RecursiveCheckMethod(seenMethods, method);
+      } else {
+        return RuleResult.DoesNotApply;
+      }
+      return Runner.CurrentRuleResult;
+    }
+
+    public RuleResult RecursiveCheckMethod(HashSet<MethodDefinition> seenMethods, MethodDefinition method) {
+      if (method == null || seenMethods.Contains(method)){
+        return Runner.CurrentRuleResult;
+      } else {
+        seenMethods.Add(method);
+      }
+
+      if (method.HasBody == false) {
+        return Runner.CurrentRuleResult;
+      }
+
+      foreach(Instruction instruction in method.Body.Instructions) {
+        Code code = instruction.OpCode.Code;
+        if (code != Code.Call) {
+          continue;
         }
 
-        foreach(Instruction instruction in method.Body.Instructions) {
-          Code code = instruction.OpCode.Code;
-          if (code != Code.Call) {
-            continue;
-          }
+        MethodReference def = instruction.Operand as MethodReference;
+        if (def == null) {
+          continue;
+        }
 
-          MethodReference def = instruction.Operand as MethodReference;
-          if (def == null) {
-            continue;
-          }
+        if (def.DeclaringType.Name == "LQDB") {
+          string message = String.Format(CultureInfo.CurrentCulture, "Don't hit LQDB in an update loop.");
+          Runner.Report( method, instruction, Severity.Critical, Confidence.Total, message );
 
-          if (def.DeclaringType.Name == "LQDB") {
-            string message = String.Format(CultureInfo.CurrentCulture, "Don't hit LQDB in an update loop.");
-            Runner.Report( method, instruction, Severity.Critical, Confidence.Total, message );
-
-            continue;
-          }
+          continue;
+        } else {
+          this.RecursiveCheckMethod(seenMethods, def.Resolve());
         }
       }
 
